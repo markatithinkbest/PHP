81a9ac3bc9f76093eec44223b4f20e8965a2dd32
Mark Chen <mark@ithinkbest.com>
2015-05-08 09:12:48 +0800

    follow the way to loop res

--- bin001/task_get_game_id.php
+++ bin001/task_get_game_id.php
@@ -1,4 +1,5 @@
 <?php
+
 // filename: task_get_game_id.php
 require ('task_get_game_id_pre.php');
 
@@ -11,12 +12,19 @@ $db->select('game_header', 'game_id,p1_id,state_id', NULL, 'state_id<99', NULL);
 
 
 if ($db->numRows() > 0) {
-$where_clause="(p1_id=$bin_id OR p2_id=$bin_id) AND state_id<99";
+    $where_clause = "(p1_id=$bin_id OR p2_id=$bin_id) AND state_id<99";
     $db->select('game_header', 'game_id,p1_id,state_id', NULL, $where_clause, NULL);
     $sql = $db->getSql();
     echo $sql;
+
+    $res = $db->getResult();
+print_r($res);
     if ($db->numRows() > 0) {
-        $arr = array('ans' => 'yes', 'todo' => 'to continue unfinished game');
+        foreach($res as $output){
+        echo $output["game_id"]."%%%%%%%%%%%%%%%%%";
+        }
+
+        $arr = array('ans' => 'yes', 'game_id' => $rec[0]['game_id'], 'todo' => 'to continue unfinished game');
     } else {
         $arr = array('ans' => 'yes', 'todo' => 'to join an open game');
     }
@@ -26,7 +34,6 @@ $where_clause="(p1_id=$bin_id OR p2_id=$bin_id) AND state_id<99";
     $res = $db->getResult();
 //echo json_encode($res);
 //print_r($res);
-    $arr = array('ans' => 'yes','game_id' => $res[0], 'p1_id' => $bin_id, 'state_id' => 1);
+    $arr = array('ans' => 'yes', 'game_id' => $res[0], 'p1_id' => $bin_id, 'state_id' => 1);
 }
 echo json_encode($arr);
-
