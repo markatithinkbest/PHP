c70f8ef285ea8447baf4e092526b0b2045389bb5
Mark Chen <mark@ithinkbest.com>
2015-05-08 08:54:07 +0800

    fix where_clause

--- bin001/task_get_game_id.php
+++ bin001/task_get_game_id.php
@@ -1,28 +1,32 @@
-﻿<?php
+<?php
 // filename: task_get_game_id.php
 require ('task_get_game_id_pre.php');
 
 
 
-include('class/db.php');
+//include('class/db.php');
 $db = new Database();
 $db->connect();
 $db->select('game_header', 'game_id,p1_id,state_id', NULL, 'state_id<99', NULL);
-//$res = $db->getResult();
-//print_r($res);
-//echo $db->getSql();
+
 
 if ($db->numRows() > 0) {
-    $arr = array('todo' => 'join an open game');
+$where_clause="(p1_id=$bin_id OR p2_id=$bin_id) AND state_id<99";
+    $db->select('game_header', 'game_id,p1_id,state_id', NULL, $where_clause, NULL);
+    $sql = $db->getSql();
+    echo $sql;
+    if ($db->numRows() > 0) {
+        $arr = array('ans' => 'yes', 'todo' => 'to continue unfinished game');
+    } else {
+        $arr = array('ans' => 'yes', 'todo' => 'to join an open game');
+    }
 } else {
-    $arr = array('todo' => 'open a new  game');
+    //  $arr = array('todo' => 'open a new  game');
     $db->insert('game_header', array('game_id' => NULL, 'p1_id' => $bin_id)); // Table name, column names and respective values
     $res = $db->getResult();
 //echo json_encode($res);
 //print_r($res);
-    $arr = array('game_id' => $res[0], 'p1_id' => $bin_id, 'state_id' => 1);
+    $arr = array('ans' => 'yes','game_id' => $res[0], 'p1_id' => $bin_id, 'state_id' => 1);
 }
 echo json_encode($arr);
 
-
-?>
--- bin001/task_get_game_id_pre.php
+++ bin001/task_get_game_id_pre.php
@@ -37,24 +37,3 @@ if (strcmp($sec_code, $known_sec_code) != 0) {
 
 
 include('class/db.php');
-$db = new Database();
-$db->connect();
-$db->select('game_header', 'game_id,p1_id,state_id', NULL, 'state_id<99', NULL);
-//$res = $db->getResult();
-//print_r($res);
-//echo $db->getSql();
-
-if ($db->numRows() > 0) {
-    $arr = array('todo' => 'join an open game');
-} else {
-    $arr = array('todo' => 'open a new  game');
-    $db->insert('game_header', array('game_id' => NULL, 'p1_id' => $bin_id)); // Table name, column names and respective values
-    $res = $db->getResult();
-//echo json_encode($res);
-//print_r($res);
-    $arr = array('game_id' => $res[0], 'p1_id' => $bin_id, 'state_id' => 1);
-}
-echo json_encode($arr);
-
-
-?>
