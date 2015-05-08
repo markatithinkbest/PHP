6ab4f0dbff9a358f981bdbac6e42dacdee5f3f32
Mark Chen <mark@ithinkbest.com>
2015-05-08 10:09:34 +0800

    hope be final to get game id

--- bin001/task_get_game_id.php
+++ bin001/task_get_game_id.php
@@ -1,42 +1,37 @@
 <?php
-
 // filename: task_get_game_id.php
-require ('task_get_game_id_pre.php');
 
-
-
-//include('class/db.php');
+require ('task_get_game_id_pre.php');  
 $db = new Database();
 $db->connect();
 
-// open games
+// list all open games
 $db->select('game_header', 'game_id,p1_id,state_id', NULL, 'state_id<99', NULL);
-
-
-if ($db->numRows() > 0) {
-    // open games with player invloved
+if ($db->numRows() > 0) {       
+    // ### Some open games ###
     $where_clause = "(p1_id=$bin_id OR p2_id=$bin_id) AND state_id<99";
     $db->select('game_header', 'game_id,p1_id,p2_id,state_id', NULL, $where_clause, NULL);
     $sql = $db->getSql();
-    echo $sql; // to debug $where_clause is necessary
+    //echo $sql; // to debug $where_clause is necessary
 
     $res = $db->getResult();
-    print_r($res); // to have a look on the result
+    //print_r($res); // to have a look on the result
     if ($db->numRows() > 0) {
+        // === 1-1 existing open game, with player invloved, to continue
         $arr = array('ans' => 'yes',
             'game_id' => $res[0]['game_id'],
             'p1_id' => $res[0]['p1_id'],
             'p2_id' => $res[0]['p2_id'],
+            'state_id' => $res[0]['state_id'],
             'desc' => 'to continue unfinished game');
     } else {
         $game_id = $res[0]['game_id'];
         $p1_id = $res[0]['p1_id'];
-
         $set_clause = array('p2_id' => $bin_id, 'state_id' => 2);
         $where_clause = "game_id=$game_id";
         $db->update('game_header', $set_clause, $where_clause); // Table name, column names and respective values
         if ($db->numRows() > 0) {
-            $arr = array('ans' => 'yes', 'todo' => 'to join an open game');
+            // === 1-1-1 existing open, without player invloved, open a new one      
             $arr = array('ans' => 'yes',
                 'game_id' => $game_id,
                 'p1_id' => $p1_id,
@@ -44,15 +39,15 @@ if ($db->numRows() > 0) {
                 'state_id' => 2,
                 'desc' => 'join an open game');
         } else {
-            $arr = array('ans' => 'no', 'todo' => 'SOMETHING IS WRONG HERE!');
+            // === 1-1-2 unexcepted situation
+            $arr = array('ans' => 'no', 'desc' => 'SOMETHING IS WRONG HERE, TO DEBUG!');
         }
     }
 } else {
-    //  $arr = array('todo' => 'open a new  game');
-    $db->insert('game_header', array('game_id' => NULL, 'p1_id' => $bin_id)); // Table name, column names and respective values
+    // ### No open game ###
+    // === 1-2 no open game, create a new one
+    $db->insert('game_header', array('game_id' => NULL, 'p1_id' => $bin_id));
     $res = $db->getResult();
-//echo json_encode($res);
-//print_r($res);
     $arr = array('ans' => 'yes', 'game_id' => $res[0], 'p1_id' => $bin_id, 'p1_id' => 0, 'state_id' => 1);
 }
 echo json_encode($arr);
