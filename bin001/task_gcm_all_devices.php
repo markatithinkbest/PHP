b3f7bd2bb8e87a2d6f9a191a29b4aa9a1347be65
Mark Chen <mark@ithinkbest.com>
2015-05-08 23:03:31 +0800

    better clean up

--- bin001/task_gcm_all_devices.php
+++ bin001/task_gcm_all_devices.php
@@ -1,5 +1,3 @@
-
-
 <?php
 // filename: task_gcm_all_devices.php
 require ('task_gcm_all_devices_pre.php');
@@ -7,51 +5,24 @@ $db = new Database();
 $db->connect();
 $db->select('bin001_id', 'reg_id', NULL, ' 1', ''); // Table name, Column Names, JOIN, WHERE conditions, ORDER BY conditions
 $res = $db->getResult();
-//print_r($res);
-//
-//$arr = array();
+
 foreach ($res as $a) {
-  //  print_r($a);
     $arr[] = $a['reg_id'];
 }
-//echo "<br>&&&&&&&&&&&&&&&&&&<br>";
 print_r($arr);
-
-//exit();
-//$res
-//while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
-//while ($row = mysql_fetch_array($res, MYSQL_NUM)) {
-////    printf("ID: %s  Name: %s", $row[0], $row[1]);  
-////    echo "<br>".$row[0]; 
-//    $arr[$myindex] = $row[0];
-//    $myindex++;
-////}
-//while ($row = mysqli_fetch_array($res)) {
-//    print_r($row);
-//
-//    $arr[$myindex++] = $row[0];
-//}
-//print_r($arr);
-
-//mysql_free_result($result);
-
-
 
 // Message to be sent
 //$message = $_POST['message'];
 $message = "ZZ馬克 testing GCM ";
-
-
-
 // Set POST variables
+
 $url = 'https://android.googleapis.com/gcm/send';
 
 $fields = array(
     'registration_ids' => $arr,
     'data' => array("message" => $message),
 );
-//                      AIzaSyATvnKoFAoOxP6lkiNmq2xhhH0hoZChg54   browser
-//                      AIzaSyCbWEy5YGvdATCaQoPBCijd_fnSa0XF_K4
+// ### NEED TO UPDATE API KEY HERE ###
 $headers = array(
     'Authorization: key=AIzaSyCbWEy5YGvdATCaQoPBCijd_fnSa0XF_K4',
     'Content-Type: application/json'
@@ -62,20 +33,14 @@ $ch = curl_init();
 
 // Set the url, number of POST vars, POST data
 curl_setopt($ch, CURLOPT_URL, $url);
-
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-
 // Execute post
 $result = curl_exec($ch);
-
 // Close connection
 curl_close($ch);
-
-//echo $result;
 ?>
 
 
