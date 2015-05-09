d9aee24b741393d248cfd88f8c7d5bd3d6f3004a
Mark Chen <mark@ithinkbest.com>
2015-05-09 11:49:16 +0800

    define("STATE_WAIT_FOR_JOIN",     -3);

--- bin001/task_get_game_id.php
+++ bin001/task_get_game_id.php
@@ -1,5 +1,27 @@
 <?php
+
 // filename: task_get_game_id.php
+
+//http://www.ithinkbest.com/b253-stateid-definition/
+
+//B253 stateId definition
+
+/**
+* === preparation ===
+* -3 (default) waiting for another player to join this game
+* -2 two more to submit their number set
+* -1 one more to submit their number set
+* 0 ready to start game
+*
+ * 
+ */
+define("STATE_WAIT_FOR_JOIN",     -3);
+define("STATE_2_MORE_NUMBER_SET",     -2);
+define("STATE_1_MORE_NUMBER_SET",     -1);
+define("STATE_0_READY_TO_START",     0);
+
+
+
 
 require ('task_get_game_id_pre.php');
 $db = new Database();
@@ -27,7 +49,7 @@ if ($db->numRows() > 0) {
     } else {
         $game_id = $res[0]['game_id'];
         $p1_id = $res[0]['p1_id'];
-        $set_clause = array('p2_id' => $bin_id, 'state_id' => -2);
+        $set_clause = array('p2_id' => $bin_id, 'state_id' => $STATE_2_MORE_NUMBER_SET);
         $where_clause = "game_id=$game_id";
         $db->update('game_header', $set_clause, $where_clause); // Table name, column names and respective values
         if ($db->numRows() > 0) {
@@ -36,20 +58,29 @@ if ($db->numRows() > 0) {
                 'game_id' => $game_id,
                 'p1_id' => $p1_id,
                 'p2_id' => $bin_id,
-                'state_id' => 2,
+                'state_id' => $STATE_2_MORE_NUMBER_SET,
                 'desc' => 'join an open game, ###GCM to inform p1');
-            
+
             // to triger gcm here
         } else {
             // === 1-1-2 unexcepted situation
-            $arr = array('ans' => 'no', 'desc' => 'SOMETHING IS WRONG HERE, TO DEBUG!');
+            $arr = array('ans' => 'no', 'desc' => '1-1-2 unexcepted situation, SOMETHING IS WRONG HERE, TO DEBUG!');
         }
     }
 } else {
     // ### No open game ###
     // === 1-2 no open game, create a new one
     $db->insert('game_header', array('game_id' => NULL, 'p1_id' => $bin_id));
-    $res = $db->getResult();
-    $arr = array('ans' => 'yes', 'game_id' => $res[0], 'p1_id' => $bin_id, 'p1_id' => 0, 'state_id' => 1);
+    if ($db->numRows() > 0) {
+        $res = $db->getResult();
+        $arr = array('ans' => 'yes', 
+            'game_id' => $res[0], 
+            'p1_id' => $bin_id, 
+            'p2_id' => 0, 
+            'state_id' => $STATE_WAIT_FOR_JOIN);
+    } else {
+        // === 1-2-2 unexcepted situation
+        $arr = array('ans' => 'no', 'desc' => '1-2-2 unexcepted situation SOMETHING IS WRONG HERE, TO DEBUG!');
+    }
 }
 echo json_encode($arr);
