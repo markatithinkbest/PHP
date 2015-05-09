aaa09d4541af6ee0bfb712041f08d01fd5b00212
Mark Chen <mark@ithinkbest.com>
2015-05-09 15:08:36 +0800

    implement debug2 to get game id

--- bin001/task_get_game_id.php
+++ bin001/task_get_game_id.php
@@ -13,10 +13,78 @@
  *
  * 
  */
-//define("STATE_WAIT_FOR_JOIN",     -3);
-//define("STATE_2_MORE_NUMBER_SET",     -2);
-//define("STATE_1_MORE_NUMBER_SET",     -1);
-//define("STATE_0_READY_TO_START",     0);
+class GcmUtilV3 {
+
+    function sendMsgToGamePlayers($game_id) {
+        require_once ('class/db.php');
+        $db = new Database();
+        $db->connect();
+        $where_clause = "game_id=$game_id";
+
+        // add p1 regId
+        $db->select('v_p1_reg_id', 'reg_id', NULL, $where_clause, ''); // Table name, Column Names, JOIN, WHERE conditions, ORDER BY conditions
+        $res = $db->getResult();
+        foreach ($res as $a) {
+            $arr[] = $a['reg_id'];
+        }
+
+        // add p2 regId
+        $db->select('v_p1_reg_id', 'reg_id', NULL, $where_clause, ''); // Table name, Column Names, JOIN, WHERE conditions, ORDER BY conditions
+        $res = $db->getResult();
+        foreach ($res as $a) {
+            $arr[] = $a['reg_id'];
+        }
+
+        $db->select('game_header', 'game_id,p1_id,p1_set,p2_id,p2_set,state_id', NULL, $where_clause, ''); // Table name, Column Names, JOIN, WHERE conditions, ORDER BY conditions
+        $res = $db->getResult();
+        foreach ($res as $a) {
+
+            //      $sample_msg = array('gamexxx' => json_encode($a));
+        }
+        print_r($res);
+        echo "<h1>---</h1>";
+        print_r($res[0]);
+        $sample_msg = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
+        //echo json_encode($arr);
+        //print_r($arr);
+
+        $url = 'https://android.googleapis.com/gcm/send';
+
+        $fields = array(
+            'registration_ids' => $arr,
+            'data' => $res[0]
+        );
+
+        // ### NEED TO UPDATE API KEY HERE ###
+        $headers = array(
+            'Authorization: key=AIzaSyCbWEy5YGvdATCaQoPBCijd_fnSa0XF_K4',
+            'Content-Type: application/json'
+        );
+
+        // Open connection
+        $ch = curl_init();
+
+        // Set the url, number of POST vars, POST data
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+        // Execute post
+        $result = curl_exec($ch);
+        // echo $result;
+        //  Close connection
+        curl_close($ch);
+        return $result;
+    }
+
+}
+
+//sample usage
+//$gcm = new GcmUtilV3();
+//$game_id=113;
+//$feedback = $gcm->sendMsgToGamePlayers($game_id);
+//echo $feedback;
 
 $state_newly_open_wait_for_join = -3;
 $state_join_game_2_more_number_set = -2;
@@ -55,33 +123,18 @@ if ($db->numRows() > 0) {
         $db->update('game_header', $set_clause, $where_clause); // Table name, column names and respective values
         if ($db->numRows() > 0) {
             // === 1-1-1 existing open, without player invloved, open a new one      
-           
-
             //===GCM=== BIG GAME HERE
-            require ('class/gcm.php');
 
-                //$arr=[];
-                $gcm_arr[] = $p1_id;
-                $gcm_arr[] = $bin_id;
-                
-          
+            $gcm = new GcmUtilV3();
+            $feedback = $gcm->sendMsgToGamePlayers($game_id);
+//echo $feedback;
 
-// Message to be sent
-//$message = $_POST['message'];
-            $gcm_msg = "$game_id IS SET FOR YOU TO SUBMIT YOUR NUMBER SET ";
-
-// Set POST variables
-
-
-            $gcm = new GcmUtil;
-            $feedback = $util->sendMsg($gcm_arr, $gcm_msg);
-
- $arr = array('ans' => 'yes',
+            $arr = array('ans' => 'yes',
                 'game_id' => $game_id,
                 'p1_id' => $p1_id,
                 'p2_id' => $bin_id,
                 'state_id' => $state_join_game_2_more_number_set,
-                'desc' => 'join an open game, ###GCM to inform p1 '.$feedback);
+                'desc' => 'join an open game, ###GCM to inform p1 ' . $feedback);
 
 
 
