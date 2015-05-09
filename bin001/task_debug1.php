8a90ce4e7a21ea5b54fc663a4777059202fa47bd
Mark Chen <mark@ithinkbest.com>
2015-05-09 13:17:11 +0800

    task_debug1 works

--- bin001/task_debug1.php
+++ bin001/task_debug1.php
@@ -1,42 +1,106 @@
 <?php
 // filename: task_gcm_all_devices.php
-require ('task_gcm_all_devices_pre.php');
-require ('class/gcm.php');
+//require ('task_gcm_all_devices_pre.php');
 
-$db = new Database();
-$db->connect();
-$db->select('bin001_id', 'reg_id', NULL, ' 1', ''); // Table name, Column Names, JOIN, WHERE conditions, ORDER BY conditions
-$res = $db->getResult();
+//require_once ('class/db.php');
 
-foreach ($res as $a) {
-    $arr[] = $a['reg_id'];
+//require_once ('class/gcm.php');
+//$db = new Database();
+//$db->connect();
+echo "xxxx";
+class GcmUtilV2 {
+
+    function sendMsgToGamePlayers($game_id, $msg) {
+        require_once ('class/db.php');
+        $db = new Database();
+        $db->connect();
+echo "xxxx";
+        $where_clause = "game_id=$game_id";
+// add p1 regId
+        $db->select('v_p1_reg_id', 'reg_id', NULL, $where_clause, ''); // Table name, Column Names, JOIN, WHERE conditions, ORDER BY conditions
+        $res = $db->getResult();
+        foreach ($res as $a) {
+            $arr[] = $a['reg_id'];
+        }
+// add p2 regId
+        $db->select('v_p1_reg_id', 'reg_id', NULL, $where_clause, ''); // Table name, Column Names, JOIN, WHERE conditions, ORDER BY conditions
+        $res = $db->getResult();
+        foreach ($res as $a) {
+            $arr[] = $a['reg_id'];
+        }
+        print_r($arr);
+
+        $url = 'https://android.googleapis.com/gcm/send';
+
+        $fields = array(
+            'registration_ids' => $arr,
+            'data' => array("message" => $msg),
+        );
+// ### NEED TO UPDATE API KEY HERE ###
+        $headers = array(
+            'Authorization: key=AIzaSyCbWEy5YGvdATCaQoPBCijd_fnSa0XF_K4',
+            'Content-Type: application/json'
+        );
+
+// Open connection
+        $ch = curl_init();
+
+// Set the url, number of POST vars, POST data
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+// Execute post
+        $result = curl_exec($ch);
+echo $result;
+//  Close connection
+        curl_close($ch);
+        return $result;
+
+    }
+
+    function sendMsg($arr, $msg) {
+        
+        
+        $url = 'https://android.googleapis.com/gcm/send';
+
+        $fields = array(
+            'registration_ids' => $arr,
+            'data' => array("message" => $msg),
+        );
+// ### NEED TO UPDATE API KEY HERE ###
+        $headers = array(
+            'Authorization: key=AIzaSyCbWEy5YGvdATCaQoPBCijd_fnSa0XF_K4',
+            'Content-Type: application/json'
+        );
+
+// Open connection
+        $ch = curl_init();
+
+// Set the url, number of POST vars, POST data
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+// Execute post
+        $result = curl_exec($ch);
+echo $result;
+//  Close connection
+        curl_close($ch);
+        return $result;
+    }
+
 }
-print_r($arr);
-
+echo "yyyy";
+//print_r($arr);
 // Message to be sent
 //$message = $_POST['message'];
-$message = "ZZ馬克 testing GCM ";
-
-// Set POST variables
-//===GCM=== BIG GAME HERE
-require ('class/gcm.php');
-
-//$arr=[];
-$p1_id=29;
-$bin_id=30;
-$game_id=97;
-$gcm_arr[] = $p1_id;
-$gcm_arr[] = $bin_id;
-
-$gcm_msg = "$game_id IS SET FOR YOU TO SUBMIT YOUR NUMBER SET ";
-
-print_r($gcm_arr);
-print_r($gcm_msg);
-
-$gcm = new GcmUtil;
-$feedback = $util->sendMsg($gcm_arr, $gcm_msg);
-
-
+$game_id = 97;
+$msg = "task_debug1.php ZZ馬克 testing GCM ";
+$gcm2 = new GcmUtilV2();
+$feedbackFromGCM = $gcm2->sendMsgToGamePlayers(97, $msg);
 ?>
 
 
@@ -47,7 +111,7 @@ $feedback = $util->sendMsg($gcm_arr, $gcm_msg);
         <meta charset="UTF-8">
     </head>
     <body>
-        <h1>DEBUG ...BIN001 APP server to GCM server result</h1>
+        <h1>BIN001 APP server to GCM server result</h1>
         <h2> <?php echo $feedbackFromGCM; ?>   </h2>
 
     </body>
